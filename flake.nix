52f0ad5097996e203b1a6ddfd3971ec6d462cd7c
Shaun Chong <pkay_@live.com>
2024-07-20 18:59:14 +0800

    chore: Added nur

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nur = { url = "github:nix-community/NUR"; };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    nur,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
